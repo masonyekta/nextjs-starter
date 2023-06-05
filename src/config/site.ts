2d671dca2a7a94bf67cc38e3063b5d64095b9357
masonyekta <3433954+masonyekta@users.noreply.github.com>
2023-06-05 16:00:25 -0400

    update packages

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+	name: 'NextJS Starter Template',
+	description: 'NextJS Starter Template',
+	mainNav: [
+		{
+			title: 'Home',
+			href: '/',
+		},
+	],
+}
